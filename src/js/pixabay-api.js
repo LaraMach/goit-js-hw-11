8374f1cc917be5584b09fc10430a343b8bc1750c
Larysa Machulianska <machulyanska@gmail.com>
2026-05-03 21:38:37 +0200

    feat: add image search gallery

---
+++ src/js/pixabay-api.js
@@ -0,0 +1,18 @@
+import axios from 'axios';
+
+const API_KEY = '55702648-64b3acfb28daf85718edb0503';
+const BASE_URL = 'https://pixabay.com/api/';
+
+export function getImagesByQuery(query) {
+  return axios
+    .get(BASE_URL, {
+      params: {
+        key: API_KEY,
+        q: query,
+        image_type: 'photo',
+        orientation: 'horizontal',
+        safesearch: true,
+      },
+    })
+    .then(response => response.data);
+}
